e1007bea31c6521ec30433f5246ae914ea74f17b
Mathilda <me@mathilda.dev>
2021-06-12 14:23:25 -0400

    Configure rotues

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :reports
-  resources :programs
-  resources :program_classifications
-  resources :institutions
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :reports, only: [ :show ]
+  resources :programs, only: [:index, :show ]
+  resources :program_classifications, only: [:index, :show ]
+  resources :institutions, only: [:index, :show ]
 end
