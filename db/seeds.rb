725adb270dcdd279bc05e2d3c83d45e0ce6f26b6
Mathilda <me@mathilda.dev>
2021-06-13 15:24:55 -0400

    Write seed script to generate fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Institution.delete_all
+# ProgramClassification.delete_all
+# Program.delete_all
+# Report.delete_all
+
+# fake_inst = Institution.find_or_create_by(
+#   opeid: 0,
+#   name: "Fake Institution",
+#   city: "Fake City",
+#   state: "FK",
+#   zip: "00000",
+#   sector: "Foreign Schools",
+#   duration_of_programs: "<2"
+# )
+
+# fake_prog_class = ProgramClassification.find_or_create_by(
+#   code: 0,
+#   name: "License in Fakery"
+# )
+
+# fake_prog = Program.create(
+#   institution: fake_inst,
+#   program_classification: fake_prog_class,
+#   credential_level: 0,
+# )
+
+# Report.create(
+#   program: fake_prog,
+#   year_published: 2015,
+#   official_pzf: "Fake",
+#   appeal_status: "",
+#   annual_de_ratio: 1.67,
+#   median_annual_debt: 5000,
+#   average_annual_earnings: 3000,
+#   annual_pzf: "Fake",
+#   discretionary_de_ratio: 1.67,
+#   average_discretionary_earnings: 3000,
+#   discretionary_pzf: "Fake",
+#   transitional_de_ratio: 1.67,
+#   median_transitional_debt: 5000,
+#   transitional_pzf: "Fake",
+#   transitional_discretionary_de_ratio: 1.67,
+#   transitional_discretionary_pzf: "Fake",
+#   mean_annual_earnings: 3000,
+#   median_annual_earnings: 3000
+# )
